2cbe7a807d78c20c5372dc3aa9c5c0f2d3b76dee
ye xingchen <ye.xingchen@zte.com.cn>
2022-08-30 08:41:24 +0000

    selinux: remove an unneeded variable in sel_make_class_dir_entries()
    Return the value sel_make_perm_files() directly instead of storing it
    in another redundant variable.
    
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: ye xingchen <ye.xingchen@zte.com.cn>
    [PM: subject line tweak]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -1918,7 +1918,6 @@ static int sel_make_class_dir_entries(struct selinux_policy *newpolicy,
 	struct selinux_fs_info *fsi = sb->s_fs_info;
 	struct dentry *dentry = NULL;
 	struct inode *inode = NULL;
-	int rc;
 
 	dentry = d_alloc_name(dir, "index");
 	if (!dentry)
@@ -1938,9 +1937,7 @@ static int sel_make_class_dir_entries(struct selinux_policy *newpolicy,
 	if (IS_ERR(dentry))
 		return PTR_ERR(dentry);
 
-	rc = sel_make_perm_files(newpolicy, classname, index, dentry);
-
-	return rc;
+	return sel_make_perm_files(newpolicy, classname, index, dentry);
 }
 
 static int sel_make_classes(struct selinux_policy *newpolicy,
