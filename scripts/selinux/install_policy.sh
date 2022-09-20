fd705d2e263e76fc5cad55903379a7728a032a07
Paul Moore <paul@paul-moore.com>
2022-09-20 14:11:11 -0400

    selinux: remove runtime disable message in the install_policy.sh script
    We are in the process of deprecating the runtime disable mechanism,
    let's not reference it in the scripts.
    
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- scripts/selinux/install_policy.sh
+++ scripts/selinux/install_policy.sh
@@ -28,8 +28,7 @@ ENABLED=`which selinuxenabled` || {
 if selinuxenabled; then
     echo "SELinux is already enabled"
     echo "This prevents safely relabeling all files."
-    echo "Boot with selinux=0 on the kernel command-line or"
-    echo "SELINUX=disabled in /etc/selinux/config."
+    echo "Boot with selinux=0 on the kernel command-line."
     exit 1
 fi
 
